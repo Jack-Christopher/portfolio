df0e32bd8f5b9fe98be802c361336b604a5ada38
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-02-22 08:29:38 -0500

    App.js relocated

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from './Navbar';
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+import MySkills from "../pages/MySkills";
+import Work from "../pages/Work";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
+
+
+import AOS from 'aos';
+import "aos/dist/aos.css";
+
+class App extends React.Component {
+    
+     componentDidMount() {
+        AOS.init({
+            duration: 500,
+            once: false,
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar />
+                <Home />
+                <About />
+                <MySkills />
+                <Work />
+                <Contact />
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
